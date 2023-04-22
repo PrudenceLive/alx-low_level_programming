7e77dbfea516ecc2b7b8fcbd24006a60668cef6b
pmprue328@gmail.com <pmprue328@gmail.com>
2023-04-21 17:51:02 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main -Entrypoint
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	/* while loop */
+	while
+		(c <= 'z');
+	{
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'Z');
+	{
+		putchar('c');
+		d++;
+	/* end while loop */
+		}
+	putchar('\n');
+	return (0);
+}
